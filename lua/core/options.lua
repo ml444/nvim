b70aad5ba5568ac6200ab8b329af96bd424d1296
mC <55684939+ml444@users.noreply.github.com>
2023-11-06 15:49:18 +0800

    Update options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -64,3 +64,21 @@ opt.foldlevelstart=99	-- 打开文件默认不折叠代码
 
 
 -- set paste             "设置粘贴模式
+
+-- 重新打开缓冲区恢复光标位置:
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.fn.setpos(".", vim.fn.getpos("'\""))
+            vim.cmd("silent! foldopen")
+        end
+    end,
+})
+
+-- 自动保存编辑的缓冲区：
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+    pattern = { "*" },
+    command = "silent! wall",
+    nested = true,
+})
